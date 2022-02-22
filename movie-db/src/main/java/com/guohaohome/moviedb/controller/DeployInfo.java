313044f11407eff97b087cf11c86bd07bfedf852
LinGuohao <iw3xv9@inf.elte.hu>
2022-02-22 12:57:08 +0100

    调试

--- movie-db/src/main/java/com/guohaohome/moviedb/controller/DeployInfo.java
+++ movie-db/src/main/java/com/guohaohome/moviedb/controller/DeployInfo.java
@@ -1,15 +1,22 @@
 package com.guohaohome.moviedb.controller;
 
 
+import com.guohaohome.moviedb.dao.MovieMapper;
+import com.guohaohome.moviedb.grpc.MoviedbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DeployInfo {
+
+    @Autowired
+    MovieMapper movieMapper;
     @RequestMapping("/")
     @ResponseBody
     public String prjInfo(){
+         //return movieMapper.getAllID().get(0);
         return "Movie-db deployed successfully";
     }
 }

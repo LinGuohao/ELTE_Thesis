cd1287c1621b14bc7099e8b0d54782e8e86b05cb
LinGuohao <iw3xv9@inf.elte.hu>
2022-02-25 03:36:29 +0100

    分离新的vue

--- movie-web/src/App.vue
+++ movie-web/src/App.vue
@@ -31,43 +31,15 @@
     </v-navigation-drawer>
 
     <v-main class="grey lighten-2">
-      <v-container>
-        <v-row>
-          <v-col v-for="n in 24" :key="n" cols="4">
-            <v-card height="400" class="mx-auto" elevation="10">
-              <v-img
-                class="white--text align-end"
-                height="200px"
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-              >
-                <v-card-title>Movie title</v-card-title>
-              </v-img>
-
-              <v-card-subtitle class="pb-0"> Director and 演员: </v-card-subtitle>
-
-              <v-card-text class="text--primary pb-0">
-                <div>Director name</div>
-                <div>演员名字</div>
-              </v-card-text>
-
-               <v-card-subtitle class="pb-0" > 评分: </v-card-subtitle>
-               <v-card-text class="text--primary pb-0">
-                <div>IMDb: 9.7 烂番茄: 100</div>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-btn color="orange" text> See more </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+        <home/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Home from './components/HomePage.vue';
 export default {
+  components: { Home },
   name: "App",
 
   data: () => ({
@@ -79,7 +51,8 @@ export default {
       ["mdi-delete", "Trash"],
       ["mdi-alert-octagon", "Spam"],
     ],
-    drawer: null,
+
+    
   }),
 };
 </script>
---
+++ movie-web/src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in numberOfID" :key="n" cols="4">
+            <v-card height="400" class="mx-auto" elevation="10">
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>Movie title</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0"> Director and 演员: </v-card-subtitle>
+
+              <v-card-text class="text--primary pb-0">
+                <div>Director name</div>
+                <div>演员名字</div>
+              </v-card-text>
+
+               <v-card-subtitle class="pb-0" > 评分: </v-card-subtitle>
+               <v-card-text class="text--primary pb-0">
+                <div>IMDb: 9.7 烂番茄: 100</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="orange" text> See more </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+  data: () => ({
+    items: [{ title: "Home" }],
+    cards: ["Today"],
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+    drawer: null,
+    numberOfID: 10,
+    movies: [{}]
+    
+  }),
+};
+</script>

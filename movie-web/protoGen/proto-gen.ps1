9fa686e4b79a39013fa3bb51bb47d8a8c43c3889
LinGuohao <iw3xv9@inf.elte.hu>
2022-05-13 20:31:53 +0200

    删除辅助文件

--- movie-web/protoGen/proto-gen.ps1
+++
@@ -1,5 +0,0 @@
-$p=Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
-echo $p
-CMD /C "protoc -I=./ moviedb.proto --js_out=import_style=commonjs:$p\movie-web\src\proto --grpc-web_out=import_style=commonjs,mode=grpcweb:$p\movie-web\src\proto"
-COPY ./moviedb.proto /Y $p\movie-web\src\proto
-
